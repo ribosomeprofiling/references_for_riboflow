23d8b1efb87f30b4ca183a378983ece0928d41a5
Hakan Ozadam <hakan.ozadam@utexas.edu>
2019-11-29 21:47:49 -0600

    initial commit

---
+++ transcriptome/mouse/v1/scripts/generate_riboflow_ref_and_annot.sh
@@ -0,0 +1,21 @@
+#!/bin/#!/usr/bin/env bash
+
+INPUT_FASTA=../raw/sequence/gencode.vM23.transcripts.fa.gz
+INPUT_GTF=../raw/annotation/gencode.vM23.annotation.gtf.gz
+INPUT_APPRIS_LIST=../raw/annotation/appris_data.principal.txt.gz
+
+mkdir -p ../riboflow_annot_and_ref
+OUTPUT_BASE=../riboflow_annot_and_ref/appris_mouse_v1
+SELECTED_FASTA="${OUTPUT_BASE}_selected.fa.gz"
+
+
+python generate_appris_reference.py -f ${INPUT_FASTA} \
+                                    -g ${INPUT_GTF} \
+                                    -a ${INPUT_APPRIS_LIST} \
+                                    -o ${OUTPUT_BASE}
+
+echo "#################################"
+echo "Generating Bowtie2 Reference"
+echo "#################################"
+
+bowtie2-build ${SELECTED_FASTA} ${OUTPUT_BASE}_selected
